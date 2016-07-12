1b41157d2741b4f10d5fec6e6d5420a524da6890
fxh90 <fengzhonghe@fengzhonghedeMacBook-Pro.local>
2016-07-12 11:45:54 +0800

    python multiplication table

--- multiplication.py
+++ multiplication.py
@@ -4,21 +4,10 @@ print'请输入乘法表阶数。\nPlease enter the order of the multiplication 
 order = input()
 a = 1
 b = 1
-print 'Standard form? 1 for yes, 0 for no'
-decision = input()
-while decision != 1 and decision != 0:
-	print 'Input not valid. Please enter 1 or 0'
-	decision = input()
-while a <= order:
-	L = []
-	while b <= order:
-		L = L + [a*b]
-		if decision:
-			if a*b < 10:
-				L = L + ['xx']
-			else:
-				L = L + ['x']
-		b = b + 1
-	print L
+for a in range(1,order+1):
+	while b in range(1,order+1):
+		print a*b,
+		b += 1
+	print
 	b = 1
-	a = a + 1
+	a += 1
