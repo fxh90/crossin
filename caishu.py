0ca471dcaf3899c9cd38ac448f42e79f0f89ac81
fxh90 <fengzhonghe@fengzhonghedeMacBook-Pro.local>
2016-07-12 11:47:27 +0800

    \n formatting

--- caishu.py
+++ caishu.py
@@ -1,39 +1,18 @@
 #!/usr/bin/python
 # coding=UTF-8
-print '''
-请输入一个1～100的整数。
-Please enter an interger in the interval from 1 to 100.
-'''
+print '\n请输入一个1～100的整数。\nPlease enter an interger in the interval from 1 to 100.\n'
+counter = 0
 import random
 a = random.randint(1,100)
 b = input()
-for c in range(1,100):
+counter = counter + 1
+while b != a:
 	if b > 100 or b < 1:
-		print '''
-你输入的数不在范围内。
-Your guess is out of range.
-'''
-		b = input()
+		print '\n你输入的数不在范围内。\nYour guess is out of range.\n'
 	elif b < a:
-		print '''
-太小了，请再猜一次。
-Your guess is too small. Please have another guess.
-'''
-		b = input()
+		print '\n太小了，请再猜一次。\nYour guess is too small. Please have another guess.\n'
 	elif b > a:
-		print '''
-太大了，请再猜一次。
-Your guess is too large. Please have another guess.
-'''
-		b = input()
-	elif b == a:
-		print '''
-恭喜您猜对了！答案是'''
-		print a
-		print '''
-Congratulations! Your guess is correct! The auswer is'''
-		print a
-		break
-	else:
-		print '请重新输入 Please retry'
-		b = input()
+		print '\n太大了，请再猜一次。\nYour guess is too large. Please have another guess.\n'
+	b = input()
+	counter = counter + 1
+print '\n恭喜您猜对了！答案是''',a,'！您在',counter,'次内猜中了！\nCongratulations! Your guess is correct! The auswer is',a,'! You got the answer in',counter,'times!\n'
