35ff3610e3493086e7af313eb199a1f0340b7cc9
fxh90 <fengzhonghe@fengzhonghedeMacBook-Pro.local>
2016-07-13 19:55:11 +0800

    微信红包模拟

--- hongbao.py
+++ hongbao.py
@@ -7,5 +7,5 @@ for i in range(num-1):
 	a = randint(1,int(100*amount_left-(num-i-1)))
 	a = float(a)/100
 	print '%.2f'%a
-	amount_left = amount_left - a
+	amount_left -= a
 print '%.2f'%amount_left
