21f8caa027bf826c029ac4711fd1238c326fdafe
fxh90 <fxh_90@126.com>
2016-07-14 23:45:58 +0800

    Ver2

--- string_blocking.py
+++ string_blocking.py
@@ -1,9 +1,9 @@
 import string
 def string_blocking(a):
     f = open('blocklist.txt')
-    data = f.read()
-    list = data.split('\n')
-    print list
+    list = f.readlines()
+    for i in [' ','\n','\r']:
+        a = string.replace(a,i,'')
     for i in list:
         a = string.replace(a,i,'**')
     return a
