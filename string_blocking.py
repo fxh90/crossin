dcebd3791495a9228f0e8d6d0f640e23a12157fc
fxh90 <fxh_90@126.com>
2016-07-15 10:26:47 +0800

    Ver3

--- string_blocking.py
+++ string_blocking.py
@@ -2,10 +2,12 @@ import string
 def string_blocking(a):
     f = open('blocklist.txt')
     list = f.readlines()
-    for i in [' ','\n','\r']:
-        a = string.replace(a,i,'')
-    for i in list:
-        a = string.replace(a,i,'**')
+    for n in range(list.index(list[-1])+1):
+        for i in [' ','\n','\r']:
+            list[n] = string.replace(list[n],i,'')
+    print list
+    for m in list:
+        a = string.replace(a,m,'**')
     return a
 inp = raw_input()
 print string_blocking(inp)
