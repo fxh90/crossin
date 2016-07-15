5731b4e7203dfba021852eb999071862798f443b
fxh90 <fxh_90@126.com>
2016-07-15 14:48:42 +0800

    Ver6 [中文BUG]

--- string_blocking.py
+++ string_blocking.py
@@ -1,21 +1,33 @@
 # coding = UTF-8
 def load_file():
-    global list
-    f = open('blocklist.txt')
-    list = f.readlines()
-def block(a):
-    for n in range(list.index(list[-1])+1):
-        for i in [' ','\n','\r']:
-            list[n] = list[n].replace(i,'')
+    global b
+    g = open('blocklist.txt')
+    list = g.readlines()
+    for n in range(list.index(list[-1]) + 1):
+        for i in [' ', '\n', '\r']:
+            list[n] = list[n].replace(i, '')
+    b = []
+    print list
     for m in list:
-        b = ['']
-        for c in m:
-            for d in range(b.index(b[-1])+1):
-                b.append(b[d] + c.upper())
-                b[d] += c
-        for e in b:
-            a = a.replace(e,'*' * len(m.decode('utf-8')))
+        print m.isalpha()
+        if m.isalpha() == 1:
+            f = ['']
+            for c in m:
+                for d in range(f.index(f[-1]) + 1):
+                    f.append(f[d] + c.upper())
+                    f[d] += c
+            b += f
+        else:
+            b += m.isalpha()
+    print b
+
+
+def block(a):
+    for e in b:
+        a = a.replace(e, '*' * len(e.decode('utf-8')))
     return a
+
+
 load_file()
 while True:
     inp = raw_input('Please enter your text. Press "ENTER" to exit.\n')
