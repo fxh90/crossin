b8f7f438696e5eb5fd3445b63c1ddcbf04e68c9f
fxh90 <fxh_90@126.com>
2016-07-15 10:54:45 +0800

    Ver4

--- string_blocking.py
+++ string_blocking.py
@@ -1,13 +1,18 @@
-import string
-def string_blocking(a):
+# coding = UTF-8
+def load_file():
+    global list
     f = open('blocklist.txt')
     list = f.readlines()
+def block(a):
     for n in range(list.index(list[-1])+1):
         for i in [' ','\n','\r']:
-            list[n] = string.replace(list[n],i,'')
-    print list
+            list[n] = list[n].replace(i,'')
     for m in list:
-        a = string.replace(a,m,'**')
+        a = a.replace(m,'*' * len(m.decode('utf-8')))
     return a
-inp = raw_input()
-print string_blocking(inp)
+load_file()
+while True:
+    inp = raw_input('Please enter your text. Press "ENTER" to exit.\n')
+    if not inp:
+        break
+    print block(inp)
